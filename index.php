93f070afda14d0e34b38eb5ac441ecd8642c21fe
ramon <ramonltorres@gmail.com>
2014-10-25 15:00:02 +0000

    added mysql password file

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ $method = isset($_GET['method']) ? $_GET['method'] : "";
 $value = isset($_GET['value']) ? $_GET['value'] : "";
 
 //preload error handler
+require_once "../databaseCredentials.php";
 require_once "classes/ErrorHandlerClass.php";
-require_once 'classes/MysqlClass.php';
-require_once 'classes/FaceRecognitionClass.php';
-require_once 'classes/UserClass.php';
+require_once "classes/MysqlClass.php";
+require_once "classes/FaceRecognitionClass.php";
+require_once "classes/UserClass.php";
 
 //load service file only
-require_once $service . '.php';
+require_once $service . ".php";
 
 ?>
